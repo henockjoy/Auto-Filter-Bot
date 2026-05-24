689582d8d362b17143ae464da779f59ab7ff1a3e
Henock Joy <89825644+henockjoy@users.noreply.github.com>
2026-05-24 14:54:02 +0400

    Update info.py

--- info.py
+++ info.py
@@ -83,9 +83,6 @@ FILES_DATABASE_URL = environ.get('FILES_DATABASE_URL', "")
 if len(FILES_DATABASE_URL) == 0:
     logger.error('FILES_DATABASE_URL is missing, exiting now')
     exit()
-SECOND_FILES_DATABASE_URL = environ.get('SECOND_FILES_DATABASE_URL', "")
-if len(SECOND_FILES_DATABASE_URL) == 0:
-    logger.info('SECOND_FILES_DATABASE_URL is empty')
 DATABASE_NAME = environ.get('DATABASE_NAME', "unix")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'yoonbot')
 
