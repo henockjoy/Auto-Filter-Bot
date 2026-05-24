f615e3f70d4eda6baf448a220d3f04597b4ecce0
Henock Joy <89825644+henockjoy@users.noreply.github.com>
2026-05-24 15:21:32 +0400

    Change page title from 'HA Bots - Entertainment' to 'Filmotainment'

--- web/utils/render_template.py
+++ web/utils/render_template.py
@@ -11,7 +11,7 @@ webapp_template = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <title>HA Bots - Entertainment</title>
+    <title>Filmotainment</title>
     <meta name="description" content="Browse trending movies and TV shows. Find and download files instantly.">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet">
