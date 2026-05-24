ae25aaa1240477df164a9deb84cd697a04bbc6c9
Henock Joy <89825644+henockjoy@users.noreply.github.com>
2026-05-24 22:51:58 +0400

    Update info.py

--- info.py
+++ info.py
@@ -131,7 +131,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "https://combative-zonda-filmotainment-21dd03cd.koyeb.app/")
+URL = environ.get("URL", "https://auto-filter-bot-9hkj.onrender.com")
 if len(URL) == 0:
     logger.error('URL is missing, exiting now')
     exit()
