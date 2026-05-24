bd0d5c44f02be3036dc387868aef60c685d991af
Henock Joy <89825644+henockjoy@users.noreply.github.com>
2026-05-24 17:45:16 +0400

    Fix caption formatting in edit_message_media

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -570,7 +570,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('« ʙᴀᴄᴋ', callback_data='about')
         ]]
         await query.edit_message_media(
-            InputMediaPhoto(random.choice(PICS), caption=script.STATUS_TXT.format(users, prm, chats, used_data_db_size, files, used_files_db_size, secnd_files, secnd_files_db_used_size, uptime)),
+            InputMediaPhoto(random.choice(PICS), caption=script.STATUS_TXT.format(users, prm, chats, used_data_db_size, files, used_files_db_size, uptime)),
             reply_markup=InlineKeyboardMarkup(buttons)
         )
     
